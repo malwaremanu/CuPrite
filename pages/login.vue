05300a982d10ebb1e277d78ebfdb4011938c789a
Manupal Choudhary <tech.manujpr@gmail.com>
2022-07-06 16:02:30 +0530

    Init

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Login page
+
+
+        <hr />
+
+
+        
+    <p class="p-4 bg-white">
+        <nuxt-link to="/">Go Back to homepage</nuxt-link>    
+    </p>
+
+    </div>
+</template>
