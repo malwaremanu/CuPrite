9fb697c41997d0d1f53f258d8543857114e81602
Manupal Choudhary <tech.manujpr@gmail.com>
2022-07-21 11:46:47 +0000

    Masters Editing

--- pages/login.vue
+++ pages/login.vue
@@ -8,20 +8,24 @@
       </center>
 
       <div class="px-8 py-3 bg-white rounded-xl lg:w-1/2">
-        <div class="text-gray-900 text-center pt-2 text-xl">
-            Login Panel
-        </div>
+        <div class="text-gray-900 text-center pt-2 text-xl">Login Panel</div>
 
         <div class="py-2">
-          <div class="text-gray-400">
-            Email ID / Username
-          </div>
-          <input type="text" class="px-4 py-2 border border-gray-400 rounded-lg w-full" v-model="username" />
+          <div class="text-gray-400">Email ID / Username</div>
+          <input
+            type="text"
+            class="px-4 py-2 border border-gray-400 rounded-lg w-full"
+            v-model="username"
+          />
         </div>
 
         <div class="py-2">
           <div class="text-gray-400">Password</div>
-          <input type="password" class="px-4 py-2 border border-gray-400 rounded-lg w-full" v-model="password" />
+          <input
+            type="password"
+            class="px-4 py-2 border border-gray-400 rounded-lg w-full"
+            v-model="password"
+          />
 
           <div class="text-right">
             <span
@@ -31,16 +35,19 @@
           </div>
         </div>
 
-        <div class="text-center text-xs lower"> {{ popup_msg }} </div>
+        <div
+          v-show="popup_msg"
+          class="text-center text-xs lower p-1 border rounded-md"
+        >{{ popup_msg }}</div>
 
         <div class="py-2" @click="login">
-            <Buttonprimary title="Login" />
+          <Buttonprimary title="Login" />
         </div>
 
         <div class="py-2">
-            <nuxt-link to="/register">
-                <Buttonprimary title="I don't have an account" />
-            </nuxt-link>
+          <nuxt-link to="/register">
+            <Buttonprimary title="I don't have an account" />
+          </nuxt-link>
         </div>
       </div>
     </div>
@@ -50,39 +57,47 @@
 </template>
 
 <script>
-
-var url = "https://c.suryamines.com/cuprite/"
+var url = 'https://c.suryamines.com/cuprite/'
 export default {
-  data(){
+  data() {
     return {
-      username : 'manupal',
-      password : '',
-      popup_msg : 'Sample msg',
+      username: 'manupal',
+      password: '',
+      popup_msg: '',
     }
   },
   methods: {
     walert() {
       alert('We will work on it later on.')
     },
-    login(){
+    login() {
       let self = this
-      const axios = require('axios').default;
+      const axios = require('axios').default
       console.log(this.username)
       console.log(this.password)
-      
-      axios.post(url + 'user/login', {
-        username: this.username,
-        password: this.password
-      })
-      .then(function (response) {
-        console.log('this is response', response.data.msg)
-        self.popup_msg = response.data.msg
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
+
+      axios
+        .post(url + 'user/login', {
+          username: this.username,
+          password: this.password,
+        })
+        .then(function (response) {
+          console.log('this is response', response.data.msg)
+          self.popup_msg = response.data.msg
+          if(response.data.status == 'error'){
+              alert(response.data.msg)
+          }
+          if(response.data.status == 'success'){
+              //alert(response.data.msg)
+              window.location.replace('/masters')
+          }
+          
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
   },
 }
 </script>
--- pages/masters/index.vue
+++ pages/masters/index.vue
@@ -29,7 +29,7 @@
         <div>
           <div class="p-2 text-gray-900">
             <button
-              @click="party_popup('show')"
+              @click="party_popup('show', 'Create')"
               class="flex items-center gap-1 font-semibold bg-white px-4 py-2 rounded-full hover:bg-slate-100 active:bg-slate-200"
             >
               <svg
@@ -54,29 +54,79 @@
       </div>
 
       <div class="p-4 overflow-x-auto">
+        <button @click="get_data">Refresh Data</button>
         <table class="w-full">
           <tr>
             <th>Name</th>
-            <th>Address</th>            
-            <th>Email</th>            
+            <th>Address</th>
+            <th>Email</th>
             <th>Under Company</th>
             <th>Added By</th>
-            <th> Action </th>
+            <th>Action</th>
           </tr>
 
           <tr v-for="party in parties" :key="party.p_uuid">
-            <td>              
-              {{ party.p_name }} 
+            <td>
+              {{ party.p_name }}
               <i v-show="!party.p_is_active" class="text-red-300">
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path></svg>
+                <svg
+                  class="w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
               </i>
             </td>
-            <td>{{ party.p_address }}</td>            
+            <td>{{ party.p_address }}</td>
             <td>{{ party.p_email }}</td>
             <td>{{ party.p_company }}</td>
-            <td>{{ party.p_author }} <br> {{ party.p_date }}</td>
             <td>
+              {{ party.p_author }}
+              <br />
+              {{ party.p_date }}
+            </td>
+            <td>
+              <button @click="delete_data(party.p_uuid)">
+                <svg
+                  class="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+              </button>
 
+              <button @click="party_popup('show', 'Edit', party)">
+                <svg
+                  class="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                  />
+                </svg>
+              </button>
             </td>
           </tr>
         </table>
@@ -128,15 +178,14 @@
                   <h3
                     class="text-lg leading-6 font-medium text-gray-300"
                     id="modal-title"
-                  >Create Party</h3>
+                  >{{ edit_create }} Party</h3>
                   <div class="mt-2">
                     <div class="text-sm text-gray-500">
-
                       <div class="text-white mb-2">
                         <label class="text-xs p-1 text-left">Name</label>
                         <input
                           type="text"
-                          :value="temp_party.p_name"
+                          v-model="temp_party.p_name"
                           class="px-4 py-2 border text-gray-50 bg-gray-700"
                         />
                       </div>
@@ -144,15 +193,16 @@
                       <div class="text-white mb-2">
                         <label class="text-xs p-1 text-left">Address</label>
                         <textarea
+                          v-model="temp_party.p_address"
                           class="px-4 py-2 border text-gray-50 bg-gray-700 w-full rounded-md"
-                        >{{ temp_party.p_address }}</textarea>
+                        ></textarea>
                       </div>
 
                       <div class="text-white mb-2">
                         <label class="text-xs p-1 text-left">Author</label>
                         <input
                           type="text"
-                          :value="temp_party.p_author"
+                          v-model="temp_party.p_author"
                           class="px-4 py-2 border text-gray-50 bg-gray-700"
                         />
                       </div>
@@ -161,7 +211,7 @@
                         <label class="text-xs p-1 text-left">Email</label>
                         <input
                           type="text"
-                          :value="temp_party.p_email"
+                          v-model="temp_party.p_email"
                           class="px-4 py-2 border text-gray-50 bg-gray-700"
                         />
                       </div>
@@ -170,7 +220,7 @@
                         <label class="text-xs p-1 text-left">Company</label>
                         <input
                           type="text"
-                          :value="temp_party.p_company"
+                          v-model="temp_party.p_company"
                           class="px-4 py-2 border text-gray-50 bg-gray-700"
                         />
                       </div>
@@ -178,11 +228,13 @@
                       <div class="text-white mb-2">
                         <label class="text-xs p-1 text-left">Status</label>
                         <select
+                          v-model="temp_party.p_is_active"
+                          :selected="temp_party.p_is_active"
                           type="select"
                           class="px-4 py-2 border text-gray-50 bg-gray-700 rounded-md"
                         >
-                          <option>Active</option>
-                          <option>Inactive</option>
+                          <option value="true">Active</option>
+                          <option value>Inactive</option>
                         </select>
                       </div>
                     </div>
@@ -193,9 +245,18 @@
             <div class="bg-gray-900 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
               <button
                 type="button"
-                @click="cons(temp_party)"
+                v-show="edit_create == 'Create'"
+                @click="post_data()"
                 class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-600 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:ml-3 sm:w-auto sm:text-sm"
               >Save</button>
+
+              <button
+                v-show="edit_create == 'Edit'"
+                type="button"
+                @click="edit_data(temp_party.p_uuid)"
+                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-600 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:ml-3 sm:w-auto sm:text-sm"
+              >Update</button>
+
               <button
                 type="button"
                 @click="party_popup('close')"
@@ -211,143 +272,226 @@
 </template>
 
 <script>
-  var url = 'http://127.0.0.1:56626/'
-  export default {
-    data() {
-      return {
-        add_party_modal: false,
-        password: '',
-        temp_party: {
-          p_uuid: '4f30ca65-2853-4d45-af4a-cd2f23d34679',
-          p_name: 'Name of Party1',
-          p_address: 'jaipur',
-          p_author: 'manu',
-          p_email: 'test2@example.com',
-          p_date: '30-30-2022',
-          p_company: 'ibps',
-          p_is_active: true,
-        },
-        parties: [
-            {
-              p_uuid: '4f30ca65-2853-4d45-af4a-cd2f23d34679',
-              p_name: 'Name of Party1',
-              p_address: 'jaipur',
-              p_author: 'manu',
-              p_email: 'test2@example.com',
-              p_date: '30-30-2022',
-              p_company: 'ibps',
-              p_is_active: false,
-            },
-            {
-              p_uuid: '35daf259-95d6-4fa5-a1b2-51665198829d',
-              p_name: 'Name of Party2',
-              p_address: 'lucknow',
-              p_author: 'anubhav',
-              p_email: 'test@test1.com',
-              p_date: '30-30-2022',
-              p_company: 'test company',
-              p_is_active: true,
-            },
-        ],
+var url = 'https://c.suryamines.com/cuprite/'
+export default {
+  data() {
+    return {
+      add_party_modal: false,
+      password: '',
+      edit_create: '',
+      temp_party: {
+        p_name: '',
+        p_address: '',
+        p_author: '',
+        p_email: '',
+        p_date: '',
+        p_company: '',
+        p_is_active: false,
+      },
+      parties: [],
+    }
+  },
+  mounted: function () {
+    this.get_data() //method1 will execute at pageload
+  },
+  methods: {
+    party_popup(x, y, z) {
+      if (x == 'show' && y == 'Create') {
+        this.add_party_modal = true
+        this.temp_party = {}
+        this.edit_create = y
+      } else if (x == 'show' && y == 'Edit') {
+        this.add_party_modal = true
+        this.edit_create = y
+        this.temp_party = z
+      } else {
+        this.add_party_modal = false
+        this.edit_create = ''
+        this.temp_party = {}
       }
     },
-    methods: {
-      party_popup(x) {
-        if (x == 'show') {
-          this.add_party_modal = true
-        } else {
-          this.add_party_modal = false
-        }
-      },
-      login() {
-        let self = this
-        const axios = require('axios').default
-        console.log(this.username)
-        console.log(this.password)
 
-        axios
-          .post(url + 'user/login', {
-            username: this.username,
-            password: this.password,
-          })
-          .then(function (response) {
-            console.log('this is response', response.data.msg)
-            self.popup_msg = response.data.msg
-            console.log(response)
-          })
-          .catch(function (error) {
-            console.log(error)
-          })
-      },
-
-      cons(x) {
-        console.log(x)
-      },
+    get_data() {
+      let self = this
+      const axios = require('axios').default
+      axios
+        .get(url + 'masters/parties')
+        .then(function (response) {
+          console.log('this is response', response.data.msg)
+          self.parties = response.data
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     },
-  }
+
+    delete_data(x) {
+      let self = this
+      const axios = require('axios').default
+      axios
+        .delete(url + 'masters/parties/' + x)
+        .then(function (response) {
+          console.log('this is response', response.data)
+          axios
+            .get(url + 'masters/parties')
+            .then(function (response) {
+              console.log('this is response', response.data.msg)
+              self.parties = response.data
+              console.log(response)
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
+          alert(response.data.msg)
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+
+    post_data() {
+      let self = this
+      const axios = require('axios').default
+      axios
+        .post(url + 'masters/parties', {
+          p_name: this.temp_party.p_name,
+          p_address: this.temp_party.p_address,
+          p_author: this.temp_party.p_author,
+          p_is_active: Boolean(this.temp_party.p_is_active),
+          p_email: this.temp_party.p_email,
+          p_company: this.temp_party.p_company,
+        })
+        .then(function (response) {
+          console.log('this is response', response.data.msg)
+          self.popup_msg = response.data.msg
+          axios
+            .get(url + 'masters/parties')
+            .then(function (response) {
+              console.log('this is response', response.data.msg)
+              self.parties = response.data
+              self.add_party_modal = false
+              console.log(response)
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
+
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+
+    edit_data(puuid) {
+      let self = this
+      const axios = require('axios').default
+      axios
+        .put(url + 'masters/parties/' + puuid, {
+          p_uuid: puuid,
+          p_name: this.temp_party.p_name,
+          p_address: this.temp_party.p_address,
+          p_author: this.temp_party.p_author,
+          p_is_active: Boolean(this.temp_party.p_is_active),
+          p_email: this.temp_party.p_email,
+          p_company: this.temp_party.p_company,
+        })
+        .then(function (response) {
+          console.log('this is response', response.data.msg)
+          self.popup_msg = response.data.msg
+
+          if (response.status == 200) {
+            axios
+              .get(url + 'masters/parties')
+              .then(function (response) {
+                console.log('this is response', response.data.msg)
+                self.parties = response.data
+                self.add_party_modal = false
+                console.log(response)
+              })
+              .catch(function (error) {
+                console.log(error)
+              })
+          }
+          else {
+            alert('Please try again.')
+          }
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+    cons(x) {
+      console.log(x)
+    },
+  },
+}
 </script>
 
 <style lang="postcss" scoped>
-  /* width */
-  ::-webkit-scrollbar {
-    width: 2px;
-    height: 5px;
-  }
+/* width */
+::-webkit-scrollbar {
+  width: 2px;
+  height: 5px;
+}
 
-  /* Track */
-  ::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 1px grey;
-    border-radius: 2px;
-  }
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 1px grey;
+  border-radius: 2px;
+}
 
-  /* Handle */
-  ::-webkit-scrollbar-thumb {
-    background: gray;
-    border-radius: 2px;
-  }
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: gray;
+  border-radius: 2px;
+}
 
-  /* Handle on hover */
-  ::-webkit-scrollbar-thumb:hover {
-    background: #c9c9c9;
-  }
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #c9c9c9;
+}
 
-  input {
-    @apply border border-gray-400 w-full rounded-md py-2 px-3 text-sm;
-  }
+input {
+  @apply border border-gray-400 w-full rounded-md py-2 px-3 text-sm;
+}
 
-  table th {
-    @apply text-white text-lg p-2 border;
-  }
+table th {
+  @apply text-white text-lg p-2 border;
+}
 
-  table td {
-    @apply text-gray-200 text-xs py-1 border p-2;
-  }
+table td {
+  @apply text-gray-200 text-xs py-1 border p-2;
+}
 
-  table {
-    font-family: Arial, Helvetica, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-  }
+table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-  table td,
-  table th {
-    border: 1px solid #ddd;
-    padding: 10px;
-  }
+table td,
+table th {
+  border: 1px solid #ddd;
+  padding: 10px;
+}
 
-  table tr:nth-child(even) {
-    background-color: #1b1b1b;
-  }
+table tr:nth-child(even) {
+  background-color: #1b1b1b;
+}
 
-  table tr:hover {
-    background-color: #1f1f1f;
-  }
+table tr:hover {
+  background-color: #1f1f1f;
+}
 
-  table th {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: left;
-    background-color: #000000;
-    color: white;
-  }
+table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #000000;
+  color: white;
+}
 </style>
