d1e054aa3e91ec1f2344a756812ee06625fd7f1b
Manupal Choudhary <tech.manujpr@gmail.com>
2022-09-12 05:25:39 +0000

    API Changed

--- store/index.js
+++ store/index.js
@@ -2,9 +2,11 @@ export const state = () => ({
     // api_url: "https://6tv4vea5c9.execute-api.ap-south-1.amazonaws.com/",
     // api_url : "https://po-manu.harperdbcloud.com",
     
-    api_url : "https://cupritev1.deta.dev/cry",
-    
+    // api_url : "https://cupritev1.deta.dev/cry",    
     //api_url : "http://127.0.0.1:8000/ncry",
+
+    api_url : "https://6tv4vea5c9.execute-api.ap-south-1.amazonaws.com",
+
 
     SEMHKAT : {
         url : 'semhkat'
