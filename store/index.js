d13094f412196a1b08f7da0dcb11c6e37d114669
Manupal Choudhary <hi@manupal.dev>
2022-09-23 12:26:06 +0000

    Revert to Suryamines

--- store/index.js
+++ store/index.js
@@ -1,7 +1,7 @@
 export const state = () => ({        
     // api_url : "https://harper.suryamines.com/cry",  
-    // api_url : "https://prod.cuprite.suryamines.com",      
-    api_url : "http://127.0.0.1:8000",      
+    api_url : "https://prod.cuprite.suryamines.com",      
+    // api_url : "http://127.0.0.1:8000",      
     SEMHKAT : {
         url : 'semhkat'
     }
