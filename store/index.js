e74d2a5633513ba19fe222824a48ec2c6c4d6ef6
Manupal Choudhary <hi@manupal.dev>
2022-09-23 12:27:10 +0000

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,5 +1,4 @@
-export const state = () => ({        
-    // api_url : "https://harper.suryamines.com/cry",  
+export const state = () => ({    
     api_url : "https://prod.cuprite.suryamines.com",      
     // api_url : "http://127.0.0.1:8000",      
     SEMHKAT : {
